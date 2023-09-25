63731681117efb6b62b2b58b1802286f386231d8
yonokuso <qltenwkd@naver.com>
2023-09-25 20:09:18 +0900

    Initial commit

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    Animator anim;
+    SpriteRenderer spriteRenderer;
+    CapsuleCollider2D capsuleCollider;
+
+    public int nextMove;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+        Invoke("Think", 5);   //delay Think 5sec
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Move
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+
+        //Platform Check
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove*0.3f, rigid.position.y);
+        Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
+        if (rayHit.collider == null)
+            Turn();
+    }
+
+    //change nextmove
+    //Àç±ÍÇÔ¼ö
+    void Think()
+    {
+        //set next active
+        nextMove = Random.Range(-1, 2);
+
+        //sprite animation
+        anim.SetInteger("Walk Speed", nextMove);
+
+        //filp sprite
+        if(nextMove != 0)   //when nextmove = 0, filp ??
+            spriteRenderer.flipX = nextMove == 1;
+
+        //Recursive
+        float nextThinkTime = Random.Range(2f, 5f); //Random Think Time
+        Invoke("Think", nextThinkTime);   //delay Think 5sec
+    }
+
+    void Turn()
+    {
+        nextMove = nextMove * -1;
+        spriteRenderer.flipX = nextMove == 1;
+
+        CancelInvoke();
+        Invoke("Think", 2);
+    }
+
+    public void OnDamaged()
+    {
+        // Sprite Alpha 
+        spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+        // Sprite Filp Y
+        spriteRenderer.flipY = true;
+        // Collider Disable
+        capsuleCollider.enabled = false;
+        // Die Effect Jump
+        rigid.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
+        // Destroy
+        Invoke("DeActive", 5);
+    }
+
+    void DeActive()
+    {
+        gameObject.SetActive(false);
+    }
+}
